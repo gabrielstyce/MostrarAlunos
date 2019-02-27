2c81e9e06837750baa615b262a90abc0d56560e7
Gabriel Araujo <42006404+GabrielStyce@users.noreply.github.com>
2019-02-26 22:23:10 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Aluno } from "../modelos/Alunos";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public alunos: Aluno[];
 
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Aluno[]>("http://gilsonpolito-api.herokuapp.com/alunos")
+      .subscribe(alunos => {
+        this.alunos = alunos;
+      }
+    )
+  }
 }
---
+++ src/app/modelos/Alunos.ts
@@ -0,0 +1,8 @@
+export interface Aluno {
+    id:number
+    nome:string
+    site:string
+    endereco:string
+    telefone:string
+    nota:number
+}
